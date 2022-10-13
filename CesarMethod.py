356c212c3dbd3df5454370da05323400410b1c39
Nikita_KK <n.sherstianykh@gmail.com>
2022-10-13 23:34:36 +0200

    Creating new file

---
+++ CesarMethod.py
@@ -0,0 +1,20 @@
+message = input("Введите сообщение: ")
+offset = int(input("Введите сдвиг: "))
+encoded_message = ""
+for ch in message:
+    if ord(ch)>=65 and ord(ch)<=90:
+        pos = ord(ch) - ord("A")
+        pos = (pos+offset)%26
+        new_char = chr(pos + ord("A"))
+        encoded_message += new_char 
+        continue
+    elif ch != " " and ch!="!":
+        pos = ord(ch) - ord("a")
+        pos = (pos+offset)%26
+        new_char = chr(pos + ord("a"))
+    elif ch=="!":
+        new_char = ch
+    else:
+        new_char = " "
+    encoded_message = encoded_message+new_char
+print(encoded_message)
