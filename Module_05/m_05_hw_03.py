4203b0e890fbed0dda01915e3968ef714ef8b0ce
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2022-10-16 12:59:53 +0200

    New homework_03

---
+++ Module_05/m_05_hw_03.py
@@ -0,0 +1,16 @@
+def sanitize_phone_number(phone):
+    ''' For working with one number'''
+    return phone.replace('(', '').replace(')', '').replace('-', '').replace('+', '').replace(' ', '')
+
+
+def sanitize_phone_numbers_list(phone):
+    '''Another desicion for list with numbers'''
+    return [new_number.replace('(', '').replace(')', '').replace('-', '').replace('+', '').replace(' ', '') for new_number in phone]
+
+
+phones_numbers = ["    +38(050)123-32-34", "     0503451234",
+                  "(050)8889900",
+                  "38050-111-22-22",
+                  "38050 111 22 11   "]
+
+print(sanitize_phone_numbers_list(phones_numbers))
