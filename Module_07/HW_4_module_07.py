7b32894e35b2160fd329c5ad52f596d92d49fd4c
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2022-11-04 18:37:38 +0100

    HW_04 in 7th Module

---
+++ Module_07/HW_4_module_07.py
@@ -0,0 +1,37 @@
+import re
+
+
+def is_integer(s):
+    if len(s) == 0:
+        return False
+    s = s.strip()
+    if s.isdigit() or s[1:].isdigit() and s[0] in ('+', '-'):
+        return True
+    else:
+        return False
+
+# Another case with regular expression
+
+
+def is_integer_reg(s):
+    if len(s) == 0:
+        return False
+    s = s.strip()
+    if re.search('[\+|\-|\d]\d+$', s):
+        return True
+    else:
+        return False
+
+
+# Testing
+print(is_integer('+34bbbbbbbbb     '))
+print(is_integer('+34    '))
+print(is_integer('-34bbbbbbbbb     '))
+print(is_integer('   +34 *@bb     '))
+print(is_integer('099     '))
+
+print(is_integer_reg('+34'))
+print(is_integer_reg('-34'))
+print(is_integer_reg('+3433!'))
+print(is_integer_reg('+34GT'))
+print(is_integer_reg('+34R%T'))
