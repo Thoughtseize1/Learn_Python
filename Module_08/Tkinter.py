46500c40fa16bfe83a4515e2ffc0ea0442388745
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2022-11-19 01:25:44 +0100

    Create a Forecast app
    Learning Tkinter and creating an app

---
+++ Module_08/Tkinter.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("METANIT.COM")
+root.geometry("250x500")
+
+label = ttk.Label(text="Hello METANIT.COM")
+label.pack()
+
+root.mainloop()
