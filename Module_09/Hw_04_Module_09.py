eeb0caf1d5e28dfca41352403c97a17e8563dba1
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2022-11-21 23:34:09 +0100

    Create Hw_04_Module_09.py

---
+++ Module_09/Hw_04_Module_09.py
@@ -0,0 +1,14 @@
+def discount_price(discount):
+    def calculate_discount(price):
+        return price * (1-discount)
+    return calculate_discount
+
+
+cost_15 = discount_price(0.15)
+cost_10 = discount_price(0.10)
+cost_05 = discount_price(0.05)
+
+price = 100
+print(cost_15(price))
+print(cost_10(price))
+print(cost_05(price))
